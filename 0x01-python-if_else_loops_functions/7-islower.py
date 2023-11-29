c8c857d64588e49b61ca3554ec4f87e9e84cbeea
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-11-30 00:10:33 +0200

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 32 and ord(c) >= 125:
+    if ord(c) >= 32 and ord(c) <= 125:
         if c >= 'a' and c <= 'z':
             return (True)
-        elif c >= 'A' and c <= 'Z':
+        else:
             return (False)
     else:
         print("Traceback (most recent call last):")
