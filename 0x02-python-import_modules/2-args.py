51e60d760c984ef9100dcc9094889cdd99fa3055
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-01 01:29:44 +0200

    0x02 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, sys.argv[i]))
