18e6dfc611518d456ad9d920c498e1477932f1ff
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-01 01:18:51 +0200

    0x02 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+	import sys
     if (len(sys.argv) == 1):
-        print("{} arguments.".format(len(sys.argv)- 1))
+        print("0 arguments.")
     else:
         print("{} arguments:".format(len(sys.argv)- 1))
         for i in range(1, len(sys.argv)):
