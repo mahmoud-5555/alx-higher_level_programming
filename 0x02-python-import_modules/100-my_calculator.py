3833be9fb7af34af0e075bcc4ef4147e33799743
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-01 22:36:36 +0200

    advance 0x02 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	import calculator_1
+    import sys
+    import calculator_1
 
-	#try:
-	a = int(sys.argv[1])
-	b = int(sys.argv[3])
-	opration = sys.argv[2]
-	if opration == '+':
-	    print("{} {} {} = {}".format(a, opration, b, calculator_1.add(a, b)))
-	elif opration == '-':
-	    print("{} {} {} = {}".format(a, opration, b, calculator_1.sub(a, b)))
-	elif opration == '*':
-	    print("{} {} {} = {}".format(a, opration, b, calculator_1.mul(a, b)))
-	elif opration == '/':
-	    print("{} {} {} = {}".format(a, opration, b, calculator_1.div(a, b)))
-	#except: print("error")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    opration = sys.argv[2]
+    if opration == '+':
+        print("{} {} {} = {}".format(a, opration, b, calculator_1.add(a, b)))
+    elif opration == '-':
+        print("{} {} {} = {}".format(a, opration, b, calculator_1.sub(a, b)))
+    elif opration == '*':
+        print("{} {} {} = {}".format(a, opration, b, calculator_1.mul(a, b)))
+    elif opration == '/':
+        print("{} {} {} = {}".format(a, opration, b, calculator_1.div(a, b)))
