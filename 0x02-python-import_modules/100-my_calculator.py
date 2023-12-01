86ac24c12af96b1076f1ed65a4a4b9d73cc9f3df
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-01 23:10:05 +0200

    0x02 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,17 +5,18 @@ if __name__ == "__main__":
     try:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        opration = sys.argv[2]
+        opr = sys.argv[2]
         if opration == '+':
-            print("{} {} {} = {}".format(a, opration, b, calculator_1.add(a, b)))
+            print("{} {} {} = {}".format(a, opr, b, calculator_1.add(a, b)))
         elif opration == '-':
-            print("{} {} {} = {}".format(a, opration, b, calculator_1.sub(a, b)))
+            print("{} {} {} = {}".format(a, opr, b, calculator_1.sub(a, b)))
         elif opration == '*':
-            print("{} {} {} = {}".format(a, opration, b, calculator_1.mul(a, b)))
+            print("{} {} {} = {}".format(a, opr, b, calculator_1.mul(a, b)))
         elif opration == '/':
-            print("{} {} {} = {}".format(a, opration, b, calculator_1.div(a, b)))
+            print("{} {} {} = {}".format(a, opr, b, calculator_1.div(a, b)))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
-    except:
+    except genral Error:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
