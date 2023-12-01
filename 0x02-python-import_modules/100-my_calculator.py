abcd0971c954a0ddba0526c1e20334208e9fa7bd
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-01 22:23:46 +0200

    0x02 100 advance

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+	import calculator_1
+
+	#try:
+	a = int(sys.argv[1])
+	print(sys.argv[3])
+	b = int(sys.argv[3])
+	opration = sys.argv[2]
+	if opration == '+':
+	    print("{} {} {} = {}".format(a, opration, b, calculator_1.add(a, b)))
+	elif opration == '-':
+	    print("{} {} {} = {}".format(a, opration, b, calculator_1.sub(a, b)))
+	elif opration == '*':
+	    print("{} {} {} = {}".format(a, opration, b, calculator_1.mul(a, b)))
+	elif opration == '/':
+	    print("{} {} {} = {}".format(a, opration, b, calculator_1.div(a, b)))
+	#except: print("error")
