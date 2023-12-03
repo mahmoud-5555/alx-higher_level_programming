4fd3a2d5ae49d0496ad387f1c39d12644dbc2672
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-03 22:59:59 +0200

    0x03 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    res = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+           res += i
+    return res
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,4 +1,8 @@
-
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	for i in my_list:
-		print(i)
+	my_list[2] = -1
+
+a = [1, 5, 11, 12]
+
+print_list_integer(a)
+print(a)
