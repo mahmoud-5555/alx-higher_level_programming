4ba3c92147bb041be0dd9f7b7bd234368a79b088
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-08 01:08:58 +0200

    0x07 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """	0_add_integer
-	this module has adds two integers
+    this module has adds two integers
     function that return the sumation of
     two intgers
 """
@@ -9,11 +9,10 @@
 
 def add_integer(a, b=98):
     """	this is function that add two intgers
-    	Args: a,b inegers values
+        Args: a,b inegers values
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-        
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
@@ -21,4 +20,3 @@ def add_integer(a, b=98):
     b = int(b)
     a = int(a)
     return a + b
-	
