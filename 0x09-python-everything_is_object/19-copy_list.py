56608db08532acfecc8faaa1b8127b69e7779dc6
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-07 04:38:34 +0200

    0x09 14

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -2,6 +2,9 @@
 '''this the stat of the medule'''
 
 
-def copy_list(l):
-    '''return a new list that is a copy of l'''
-    return l.copy()
+def copy_list(l_list):  # l: is an list
+    '''
+    l: is an list
+    return a new list that is a copy of l
+    '''
+    return l_list.copy()
