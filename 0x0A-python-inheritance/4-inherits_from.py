a763679943f3b23064e21d5f0d77940a95b81aa3
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-10 21:23:26 +0200

    0x0A 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this 4-inherits_from.py"""
+
+
+def inherits_from(obj, a_class):
+    """
+        a function that returns True if the object is an instance of,
+        or if the object is an instance of a class that inherited from,
+        the specified class; otherwise False.
+     """
+    return isinstance(obj, a_class)
