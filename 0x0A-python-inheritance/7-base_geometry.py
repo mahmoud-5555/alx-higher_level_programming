b873392cbd954812f64af0382ed8ca5470d800b7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-10 23:53:50 +0200

    0x0A 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,15 +4,18 @@
 
 class BaseGeometry:
     """this is BaseGeometry class"""
+
     def area(self):
         """the plany method in the class"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
+        """integer_validator"""
         if type(name) is not str:
             raise TypeError('parameter name must be a string')
         if name is '':
             raise ValueError('name cannot be empty')
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
-        if value <= 0 :
-            raise ValueError( name + " must be greater than 0")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
