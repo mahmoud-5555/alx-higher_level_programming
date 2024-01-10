5ca4ed81ca1d1e6b51d0bcffd9c930418fd0b33c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-11 01:10:07 +0200

    0x0A 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this 10-square"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """this is the square class that inherits from the class Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+            return "[Square] {}/{}".format(self.__width, self.__height)
