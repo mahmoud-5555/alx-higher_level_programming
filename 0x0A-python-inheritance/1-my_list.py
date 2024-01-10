efd2ba0ef2b66db374acf33a907016a2bfd98997
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-10 20:37:37 +0200

    0x0A 1+ tests 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,9 @@
 
 class MyList(list):
     """A class that inherits from the built-in list class."""
-    def __init__(self):
+    def __init__(self,*args, **kwargs):
         """ Call to parent constructor"""
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
     def print_sorted(self):
         print(sorted(self))
