e8f8131c5d57d0eb3e29debea15e89f4d31a7ab8
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-11 01:41:11 +0200

    0x0A 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ my module """
 
+
 def add_attribute(obj, attr_name, attr_value):
     """Add a new attribute to an object if it's possible."""
     if hasattr(obj, '__dict__') and not hasattr(obj, attr_name):
