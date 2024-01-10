447dce9dd9915c400733e7527a0658dce35d49d7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-10 22:52:21 +0200

    0x0A 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this 6-base_geometry module """
+
+
+class BaseGeometry:
+    """this is BaseGeometry class"""
+    def area(self):
+        """the plany method in the class"""
+        raise Exception("area() is not implemented")
