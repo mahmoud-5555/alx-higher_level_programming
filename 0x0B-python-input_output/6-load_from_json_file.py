3442b86794e886acd98d3be94fd371dc815180a8
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-11 03:57:49 +0200

    0x06

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,5 +4,9 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+        function that convert json_string
+        from the file to python opject
+    """
     with open(filename, 'r', encoding='UTF8') as file:
         data = json.load(file)
