4447bd020d70bb8aa0852e6df0ed56679e8b8d80
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-11 02:54:57 +0200

    0x0B 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,8 @@ def read_file(filename=""):
     """
         function that print the contest that inside the file
     """
+    contest = ''
     with open(filename, 'r+', encoding='UTF8') as myfile:
-        print(myfile.read())
+        contest = myfile.read()
+        print(contest)
+        return contest
