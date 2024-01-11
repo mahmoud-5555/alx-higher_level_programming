c78925659840425dc12b35a0b898abd69cbc9c8e
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-01-11 03:09:46 +0200

    0x0B 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,3 +6,4 @@ def write_file(filename="", text=""):
     """function that overwrite of the file"""
     with open(filename, 'w', encoding="UTF8") as myfile:
         myfile.write(text)
+    return len(text)
