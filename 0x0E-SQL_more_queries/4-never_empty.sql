d567a07569eedc64bd169fea192388e9cb9c9953
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-15 03:26:03 +0200

    t4 sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,3 @@
- -- create new table with id not NULL 
+ -- create new table with id not NULL
+ -- this will be in sql server
 CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL, name VARCHAR(255));
