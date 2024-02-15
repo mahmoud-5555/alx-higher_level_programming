15ffb696168502171b8b005a5336dbbc1b5f6242
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-15 02:32:17 +0200

    the task

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,4 @@
 --this code should list privileges of the MySQL
 --users user_0d_1 and user_0d_2 on your server (in localhost).
--- Grant privileges to existing users only
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON *.* TO 'user_0d_1'@'localhost';
-
--- Check if 'user_0d_2' exists before granting privileges
-SELECT COUNT(*) INTO @user_0d_2_exists FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
-
--- Grant privileges to 'user_0d_2' if it exists
-IF @user_0d_2_exists > 0 THEN
-    GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON *.* TO 'user_0d_2'@'localhost';
-END IF;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
