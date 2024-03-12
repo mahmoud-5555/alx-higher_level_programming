2c8efc9554850ac725f831ac23cf7b9cfbcc93a5
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-12 22:21:14 +0200

    5 0x12

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const mystring = ['C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) { console.log(mystring[i]); }
+// this will print the elements of array from
