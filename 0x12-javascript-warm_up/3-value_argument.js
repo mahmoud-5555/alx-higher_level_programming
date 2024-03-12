344c2d37d363fa4da82236b931e8cd8e1fcb471c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-12 17:01:21 +0200

    3 0x12

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,4 +4,7 @@ const { argv } = require('node:process');
 
 if (argv[2] === undefined) {
   console.log('No argument');
-} else { console.log(argv[2]); }
+} else {
+  let i = 2;
+  while (argv[i] !== undefined) { console.log(argv[i]); i++; }
+}
