38775e34c836c29ffe8fd2aeb59867838792a73c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-13 02:59:31 +0200

    6 0x13

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+const Test = require('./5-square');
+
+class Square extends Test {
+  charPrint (c) {
+    let mywidth = '';
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      mywidth += 'c';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(mywidth);
+    }
+  }
+}
+module.exports = Square;
