93adb55b6d427447541fc552e3012e260e4a7fd5
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-13 02:21:57 +0200

    3 0x13

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,24 +2,23 @@
 
 // the Rectangel class
 class Rectangle {
-	constructor (w, h) {
-	  if ((w <= 0) || (h <= 0) || isNaN(w) || isNaN(h)) {
-		return Object.create(Rectangle.prototype);
-	  } else {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-	print(){
-		let mywidth = '';
-		for(let i = 0; i < this.width; i++) {
-			mywidth += 'X';
-		}
-		for(let i = 0; i < this.height; i++) {
-			
-			console.log(mywidth);
-		}
-	}
+  constructor (w, h) {
+    if ((w <= 0) || (h <= 0) || isNaN(w) || isNaN(h)) {
+      return Object.create(Rectangle.prototype);
+    } else {
+      this.width = w;
+      this.height = h;
+    }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    let mywidth = '';
+    for (let i = 0; i < this.width; i++) {
+      mywidth += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(mywidth);
+    }
+  }
+}
+module.exports = Rectangle;
