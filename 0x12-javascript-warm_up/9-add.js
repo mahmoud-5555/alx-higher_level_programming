6842551a56f0dcdfc7ba7afa05ac9c2a656d52c2
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-14 02:51:00 +0200

    9 0x12

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+function add (a, b) {
+  return a + b;
+}
+function isDigite (value) {
+  return /^-?\d+$/.test(value);
+}
+
+if (argv.length < 4) {
+  console.log('NAN');
+} else {
+  if (isDigite(argv[2]) && isDigite(argv[3])) {
+    const result = add(Number(argv[2]), Number(argv[3]));
+    console.log(result);
+  } else {
+    console.log('NaN');
+  }
+}
