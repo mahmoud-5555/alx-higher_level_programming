04e0e9fbc9538d8b57d2120109769a4ba6bd46b7
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-16 01:45:01 +0200

    0 0x0F

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python3
 """
 this  module for connect to the data base
-and print the status 
+and print the status
 """
 
 import sys
 import MySQLdb
 
-def connect_to_mysql(host, user, password, databases):
-	"""
-	Connect to the database
-	@Arge:
-	host: the host that the database in
-	user: the username that you wanna to assain with
-	password: the password that related to the username
-	database: the database that you wanna to connect with
-	"""
-	try:
-		mydatabases = MySQLdb.Connect(host, user, password, databases)
-	except:
-		pass
 
-	return mydatabases
+def connect_to_mysql(host, user, password, databases):
+    """
+    Connect to the database
+    @Arge:
+    host: the host that the database in
+    user: the username that you wanna to assain with
+    password: the password that related to the username
+    database: the database that you wanna to connect with
+    """
+    try:
+        mydatabases = MySQLdb.Connect(host, user, password, databases)
+    except NameError as error:
+        return None
+
+    return mydatabases
 
 
 if __name__ == "__main__":
-	argumant = sys.argv
-	mysatabase =  connect_to_mysql("localhost:3306", argumant[2], argumant[3], argumant[4])
-	dp_pointer = mysatabase.cursor()
-	dp_pointer.execute("SELECT id,name FROM states ORDER BY id;")
+    argumant = sys.argv
+    h = "localhost"
+    u = argumant[1]
+    Upass = argumant[2]
+    dp = argumant[3]
+    p = 3306
 
-	for state in dp_pointer:
-		print(state) 
- 
+    mysatabase =\
+        connect_to_mysql(host=h, port=p, user=u, password=Upass, databases=dp)
 
+    if mysatabase is None:
+        exit(1)
+    dp_pointer = mysatabase.cursor()
+    dp_pointer.execute("SELECT id,name FROM states ORDER BY id;")
+
+    for state in dp_pointer:
+        print(state)
