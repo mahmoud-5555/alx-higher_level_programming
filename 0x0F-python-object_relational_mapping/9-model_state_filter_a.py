a2c0d93df546db9bcf22341403f35a896db8103d
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-17 21:15:37 +0200

    8 0x0F

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module for printing the frist row in
+the databases from table state
+"""
+
+from model_state import Base, State
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+from re import search as se
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+
+    engine = sql.create_engine('mysql://{}:{}@localhost:\
+                               3306/{}'.format(u, p, d))
+    Base.metadata.create_all(engine)
+    id = State.id
+    s_n = State.name
+    with engine.connect() as conn:
+        result = conn.execute(sql.select(State).
+                              where((s_n).like('%a%')).order_by(id))
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
