9f90671807424d40b623be5619f6bff0426f0ab2
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-17 04:03:45 +0200

    6 0x0F

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module for class that mange databases
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+	"""databases class  to manage the state of a device."""
+	__tablename__ = 'states'
+
+	id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+	name = Column(String(128), nullable=False)
+
+	def __init__(self, name):
+		self.name = name
+
+
+engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_4_usa')
