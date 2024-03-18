45f394ed5eaa56ec83b0ab89e9b363a6a7011538
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-03-18 03:03:13 +0200

    11 0x0F

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this module for printing the frist row in
+the databases from table state
+"""
+
+from model_state import Base, State
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+from re import search as se
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+
+    engine = sql.create_engine('mysql://{}:{}@localhost:\
+                               3306/{}'.format(u, p, d))
+    with engine.connect() as conn:
+        updated_state = conn.execute(sql.update(State)
+                                     .where(State.id == 2)
+                                     .values(name= 'New Mexico'))
+        conn.commit()
