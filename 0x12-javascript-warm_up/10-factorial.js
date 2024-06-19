ade00c9ddde5423c8df3a9d7ed2752ab03241af9
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-06-19 05:09:20 +0300

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 
-function factorial(result, input){
-if (input === 0 || input === 1) {
-  return result;
-}
-else {
-  return factorial(result * input, input - 1);
- }
+function factorial (result, input) {
+  if (input <= 1) {
+    return result;
+  } else {
+    return factorial(result * input, input - 1);
+  }
 }
 
-let input = +argv[2];
-if (isNaN(input)){
+const input = +argv[2];
+if (isNaN(input)) {
   console.log(1);
 } else {
-	console.log(factorial(1, input));
+  console.log(factorial(1, input));
 }
