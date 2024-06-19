c4b0b0589653cf1913513d081d5a623e0fd4eb76
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-06-19 05:03:12 +0300

    add 8-square

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
-
-function isDigite (value) {
-	return /^-?\d+$/.test(value);
-  }
-function 
-
 const { argv } = require("node:process");
-if (isDigite(argsv[2]){
 
+function factorial(result, input){
+if (input === 0 || input === 1) {
+  return result;
+}
+else {
+  return factorial(result * input, input - 1);
+ }
+}
+
+let input = +argv[2];
+if (isNaN(input)){
+  console.log(1);
+} else {
+	console.log(factorial(1, input));
 }
